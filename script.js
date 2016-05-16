9892b1aa161dfced85883e583e3578333751951a
whedrick <william.hedrick@gmail.com>
2016-05-16 14:42:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,24 +14,16 @@ module.exports = new Script({
     start: {
         receive: (bot) => {
             return bot.say('Hi!  Welcome to the Oklahome Employment Security Commission (OESC) Automated Solution Chat (ASC)!  If you\'re having a problem, just ASC!')
-                .then(() => 'initialHelp');
+                .then(() => 'askFirstName');
         }
     },
-	
-	initialHelp: {
-		prompt: (bot) => bot.say('I can help you with several things.  Please choose one of the following:	%[I would like to file a new claim](postback:file_new_claim) %[I received a letter about my claim](postback:received_letter) %[I would like to chat with a live person](live_person) %[I would like a phone call from a representative](phone_call)'
-		receive: (bot, message) => {
-				.then(() => bot.say('You can file a new claim by visiting our website, https://unemployment.state.ok.us/'))
-				.then(() => 'speak');  
-		}
-	},
 	
 	askFirstName: { 
 		prompt: (bot) => bot.say('For the following questions, please provide the information exactly as it appears on your claim.\nWhat is your first name?'), 
 		receive: (bot, message) => { 
 			const firstName = message.text; 
 			return bot.setProp('firstName', firstName) 
-				.then(() => bot.say('Great! I\'ll call you ${firstName}')) 
+				.then(() => bot.say(`Great! I'll call you ${firstName}`)) 
 				.then(() => 'askLastName'); 
 		} 
 	}, 
@@ -41,17 +33,7 @@ module.exports = new Script({
 		receive: (bot, message) => { 
 			const lastName = message.text; 
 			return bot.setProp('lastName', lastName) 
-				.then(() => bot.say('Thank you! I understand your full name is ${firstName} ${lastName}')) 
-				.then(() => 'askSocial'); 
-		} 
-	}, 
-	
-	askSocial: { 
-		prompt: (bot) => bot.say('What is your Social Security Number?'), 
-		receive: (bot, message) => { 
-			const social = message.text; 
-			return bot.setProp('social', social) 
-				.then(() => bot.say('Thank you! I understand your SSN is ${social}')) 
+				.then(() => bot.say(`Thank you! I understand your full name is ${firstName} ${lastName}`)) 
 				.then(() => 'speak'); 
 		} 
 	}, 
@@ -83,7 +65,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say('I didn\'t understand that.').then(() => 'speak');
+                    return bot.say(`I didn't understand that.`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
