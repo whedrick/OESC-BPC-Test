3fa47143bac5b58cf06619af6ce2eb8a181ff4ce
whedrick <william.hedrick@gmail.com>
2016-05-16 13:31:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say("Hi!  Welcome to the Oklahome Employment Security Commission (OESC) Automated Solution Chat (ASC)!  If you're having a problem, just ASC!")
                 .then(() => 'speak');
         }
     },
