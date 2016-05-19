70af8bd03c8bf80de80de952d5cab511e63ab27f
whedrick <william.hedrick@gmail.com>
2016-05-19 16:23:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ module.exports = new Script({
 	checkInfo: {
 		prompt: (bot) => bot.getProp('firstName')
 			.then(() => bot.getProp('lastName'))
-			.then((firstName, lastName) => bot.say(`First Name: ${firstName}\nLast Name: ${lastName}`)), 
+			.then((lastName, firstName) => bot.say(`First Name: ${firstName}\nLast Name: ${lastName}`)), 
 		receive: (bot, message) => { 
 			return bot.say("Great!  Let me check your claim.")
 			.then(() => 'speak');
