16ce1bbada7a5c7611dabfd685213a2033f0b24b
whedrick <william.hedrick@gmail.com>
2016-05-19 15:08:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -73,8 +73,7 @@ module.exports = new Script({
 	}, 
 
 	checkInfo: {
-		prompt: (bot) => bot.getProp('firstName')
-			.then((firstName) => bot.say(`Is your first name ${firstName} %[Yes](postback:yes) %[No](postback:no)`)),
+		prompt: (bot) => bot.say('What is your Social Security Number?'), 
 		receive: (bot, message) => { 
 			return bot.say("Great!  Let me check your claim.")
 			.then(() => 'speak');
