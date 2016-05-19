ccfbd5946fe832eca38f6773e37172d8b7126455
whedrick <william.hedrick@gmail.com>
2016-05-19 13:23:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ module.exports = new Script({
 			// social = social.replaceAll("[^\\d.]", "");
 			return bot.setProp('social', social) 
 				.then(() => bot.say(`Thank you! I understand your SSN is ${social}`))
-				.then(() => bot.say(social.replaceAll("[^\\d.]", "")))
+				//.then(() => bot.say(message.text.replaceAll("[^\\d.]", "")))
 				.then(() => 'checkInfo'); 
 		} 
 	}, 
