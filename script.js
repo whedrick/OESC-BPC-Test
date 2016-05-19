9e84cb824805e21388004e349096d0f2fba64c68
whedrick <william.hedrick@gmail.com>
2016-05-19 14:37:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ module.exports = new Script({
 	}, 
 
 	checkInfo: {
-		prompt: (bot) => bot.say("Is your first name "+ firstName + " %[Yes](postback:yes) %[No](postback:no)")),
+		prompt: (bot) => bot.say('Is your first name ${firstName} %[Yes](postback:yes) %[No](postback:no)')),
 		receive: (bot, message) => { 
 			return bot.say("Great!  Let me check your claim.")
 			.then(() => 'speak');
