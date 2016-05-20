b758612b1d089f3c092e6b7e19af09986d2c505a
whedrick <william.hedrick@gmail.com>
2016-05-20 14:53:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -75,6 +75,7 @@ module.exports = new Script({
 	checkInfo: {
 		prompt: (bot) => bot.say('Let\'s review the information your provided...')
 			.then(() => {
+				var promptWords = bot.getProp('firstName');
 				return bot.say('This Works');
 			})
 			.then(() => bot.say('Is this correct? %[Yes](postback:yes) %[No](postback:no)')), 
